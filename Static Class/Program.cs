db8822aeb013a1845e0d7d06d3708e9fb94cacef
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-25 04:37:27 +0530

    Add project files.

---
+++ Static Class/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Static_Class
+{
+    static class myclass
+    {
+        static myclass()
+        {
+            Console.WriteLine("Static Constructor :");
+        }
+        public static void show()
+        {
+            Console.WriteLine("Static method  :");
+        }
+        
+       
+    } 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            myclass.show();
+        }
+    }
+}
